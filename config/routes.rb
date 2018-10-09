be676d61dc9ff98596a26548243d645f731d5522
Elvis <yinchuang94@gmail.com>
2018-10-09 10:53:16 +0800

    added routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  # get '/show' => "welcome#show" 
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+  # get '/show' => "welcome#show"
 end
