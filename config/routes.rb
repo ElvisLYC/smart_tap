ff20d4ce0af6fab8e3d37d2ff129e1d7be2f9d24
MOHD FAIZ <tenkois@gmail.com>
2018-10-09 13:25:52 +0800

    location_about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -41,3 +42,27 @@ Rails.application.routes.draw do
 
 
 end
+=======
+Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/location'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "users", only: [:create, :show, :update, :edit] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+  # get '/show' => "welcome#show"
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+  get "/users/:id/add_device" => "devices#new", as: 'devices'
+  get "/users/:id/new_subscription" => "subscriptions#new", as: 'new_subscription'
+
+  post '/users/:id' => "users#edit"
+end
+>>>>>>> location_about
