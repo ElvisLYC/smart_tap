1bb6bc567d6b004074e7524960ba609d003c4aca
AsyrafN <asyrafnaqi@yahoo.com>
2018-10-09 14:11:58 +0800

    Added users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
 private
   def user_params
     params.require(:user).permit(:email, :password, :first_name, :last_name, :gender, :city, :zipcode, :state, :country, :phone_number, :birthdate, :address)
