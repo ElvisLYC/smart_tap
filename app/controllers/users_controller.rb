1270be641ed94cba76620f8da8e3ec726fd75315
AsyrafN <asyrafnaqi@yahoo.com>
2018-10-09 17:00:28 +0800

    Added User profile edit function with layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render '/users/edit'
+    end
+
+  end
+
 private
   def user_params
     params.require(:user).permit(:email, :password, :first_name, :last_name, :gender, :city, :zipcode, :state, :country, :phone_number, :birthdate, :address)
