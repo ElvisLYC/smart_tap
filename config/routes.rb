7ab7c3ef8de1fc7bd0f1e557aa87c42b5d5764e9
Elvis <yinchuang94@gmail.com>
2018-10-11 20:59:19 +0800

    completed jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
@@ -20,5 +21,5 @@ Rails.application.routes.draw do
 
   # Cross Origin problem
   match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
-
+  mount Sidekiq::Web => '/sidekiq'
 end
