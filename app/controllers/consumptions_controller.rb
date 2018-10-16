1fcb0ed82172ff84ad1de51a54b8e7551bceea04
AsyrafN <asyrafnaqi@yahoo.com>
2018-10-16 12:43:44 +0800

    Added live chart

--- app/controllers/consumptions_controller.rb
+++ app/controllers/consumptions_controller.rb
@@ -1,20 +1,35 @@
 class ConsumptionsController < ApplicationController
 
-	# def create
-	# 	@consumption = Consumption.new
-	# 	@consumption.power = "0"
-	# 	@volt = @consumption.power
+	def create
+		# when the light is on create a new power data
+		# if the device and user id is the same it will take the data instead of creating a new data
+		# and will just update the data when close
+		
+		@consumption = Consumption.new
+		@consumption.power = 0
+		@consumption.save
+	end
 
-	# 	if (light_state == 1) do 
-	# 		volt += 1
-	# 		break if (light_state == 0)
-	# 	end
+	def update
+		# when the light is off the power data is updated to the consumption table
+		@consumption = Consumption.find(params[:power])
+		@consumption.update(params[:power])
 
-	# 	elsif @consumption.update
-			
-
-	# 	else @consumption.save
-
-	# end
+	end
 
 end
+
+
+# @consumption = Consumption.new
+# 		@consumption.power = "0"
+# 		@volt = @consumption.power
+
+# 		if (light_state == 1) do 
+# 			volt += 1
+# 			break if (light_state == 0)
+# 		end
+
+# 		elsif @consumption.update
+			
+
+# 		else @consumption.save
